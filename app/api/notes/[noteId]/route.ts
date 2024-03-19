0875bc602843f071c93f13663fc99112810d7b45
BhanukaAGH <abewickramah1@gmail.com>
2024-03-19 14:18:18 +0530

    feat: implement note crud api

---
+++ app/api/notes/[noteId]/route.ts
@@ -0,0 +1,44 @@
+import { db } from '@/lib/db'
+
+export const PUT = async (
+  req: Request,
+  { params }: { params: { noteId: number } }
+) => {
+  try {
+    const { title, content } = await req.json()
+
+    await db.note.update({
+      where: {
+        id: params.noteId.toString(),
+      },
+      data: { title, content },
+    })
+
+    return Response.json('Note updated successfully.')
+  } catch (error) {
+    return Response.json(
+      { message: 'Failed to update the note. Please try again later.' },
+      { status: 500 }
+    )
+  }
+}
+
+export const DELETE = async (
+  req: Request,
+  { params }: { params: { noteId: number } }
+) => {
+  try {
+    await db.note.delete({
+      where: {
+        id: params.noteId.toString(),
+      },
+    })
+
+    return Response.json('Note deleted successfully.')
+  } catch (error) {
+    return Response.json(
+      { message: 'Failed to delete the note. Please try again later.' },
+      { status: 500 }
+    )
+  }
+}
---
+++ app/api/notes/route.ts
@@ -0,0 +1,31 @@
+import { db } from '@/lib/db'
+
+export const GET = async () => {
+  try {
+    const data = await db.note.findMany()
+    return Response.json(data)
+  } catch (error) {
+    return Response.json({ message: 'Failed to fetch notes.' })
+  }
+}
+
+export const POST = async (req: Request) => {
+  try {
+    const { title, content } = await req.json()
+
+    if (!title) {
+      return Response.json({ message: 'title is required' }, { status: 400 })
+    }
+
+    const data = await db.note.create({
+      data: { title, content: content ?? '' },
+    })
+
+    return Response.json(data)
+  } catch (error) {
+    return Response.json(
+      { message: 'Failed to create the note. Please try again later.' },
+      { status: 500 }
+    )
+  }
+}
